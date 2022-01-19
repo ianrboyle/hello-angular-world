7fa48af4a6d4eaf94e826db0924052ef5dec1735
Ian Boyle <irboyle21@gmail.com>
2022-01-19 10:46:53 -0500

    adds courses component

---
+++ src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core"
+//properties to tell angular how this component works
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses</h2>'
+})
+// typescript class - PascalCase
+
+class CoursesComponent {
+
+}
